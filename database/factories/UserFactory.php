3729fbea0f430e7e5b559b6c229775bf03c2a2db
Seigi Takada <seiji.takada@medixserve.com>
2019-08-11 14:50:24 +0800

    Added reply factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,9 +29,21 @@ $factory->define(User::class, function (Faker $faker) {
 $factory->define(Thread::class, function (Faker $faker) {
     return [
         'user_id' => function () {
-            return factory('App\User')->create()->id;
+            return factory(User::class)->create()->id;
         },
         'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'thread_id' => function () {
+            return factory(Thread::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
         'body' => $faker->paragraph
     ];
 });
