6931d81316b69cfac0e037db664b13f8a423ce8b
Seigi Takada <seiji.takada@medixserve.com>
2019-08-11 23:37:53 +0800

    Updated with channel

--- app/Thread.php
+++ app/Thread.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $fillable = [
-        'user_id', 'title', 'body'
+        'user_id', 'title', 'channel_id', 'body'
     ];
 
     public function path()
     {
-        return '/threads/' . $this->id;
+        return "/threads/{$this->channel->slug}/{$this->id}";
     }
 
     public function replies()
@@ -23,6 +23,11 @@ class Thread extends Model
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
     }
 
     public function addReply($reply)
