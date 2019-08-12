36fb1befe16593db099c45437998a5dcc5093b71
Seigi Takada <seiji.takada@medixserve.com>
2019-08-12 23:25:48 +0800

    Fixed layout and added delete

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,38 +2,32 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        <h1>
-                            {{ $profileUser->name }}
-                            <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-                        </h1>
-                    </div>
-            
-                    <div class="card-body">
-                        @foreach ($threads as $thread)
-                            <div class="card mb-3">
-                                <div class="card-header">
-                                    <div class="d-flex bd-highlight">
-                                        <div class="bd-highlight">
-                                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: {{ $thread->title }}
-                                        </div>
-                                        <div class="ml-auto bd-highlight">
-                                            {{ $thread->created_at->diffForHumans() }}
-                                        </div>
-                                    </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1>
+                    {{ $profileUser->name }}
+                    <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+                </h1>
+
+                @foreach ($threads as $thread)
+                    <div class="card mb-3">
+                        <div class="card-header">
+                            <div class="d-flex bd-highlight">
+                                <div class="bd-highlight">
+                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: {{ $thread->title }}
                                 </div>
-                                <div class="card-body">
-                                    {{ $thread->body }}
+                                <div class="ml-auto bd-highlight">
+                                    {{ $thread->created_at->diffForHumans() }}
                                 </div>
                             </div>
-                        @endforeach
-
-                        {{ $threads->links() }}
+                        </div>
+                        <div class="card-body">
+                            {{ $thread->body }}
+                        </div>
                     </div>
-                </div>
+                @endforeach
+
+                {{ $threads->links() }}
             </div>
         </div>
     </div>
