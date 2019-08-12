b934f2b9f32a8c1a65f29a8e10abef23d4f74e04
Seigi Takada <seiji.takada@medixserve.com>
2019-08-12 23:25:59 +0800

    Fixed layout

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,31 +4,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Forum Threads</div>
-
-                <div class="card-body">
-                    @foreach ($threads as $thread)
-                        <article>
-                            <div class="d-flex bd-highlight">
-                                <h4 class="bd-highlight">
-                                    <a href="{{ $thread->path() }}">
-                                        {{ $thread->title }}
-                                    </a>
-                                </h4>
-
-                                <a href="{{ $thread->path() }}" class="ml-auto bd-highlight">
-                                    {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}
+            @foreach ($threads as $thread)
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <div class="d-flex bd-highlight">
+                            <h4 class="bd-highlight">
+                                <a href="{{ $thread->path() }}">
+                                    {{ $thread->title }}
                                 </a>
-                            </div>
-                            
-                            <div class="body">{{ $thread->body }}</div>
-                        </article>
+                            </h4>
 
-                        <hr>
-                    @endforeach
+                            <a href="{{ $thread->path() }}" class="ml-auto bd-highlight">
+                                {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="body">{{ $thread->body }}</div>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
