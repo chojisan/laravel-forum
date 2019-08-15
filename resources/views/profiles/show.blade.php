2575b287bc8a2438ddd817084700edb80247d896
Seigi Takada <seiji.takada@medixserve.com>
2019-08-15 18:41:27 +0800

    Added link to title

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -14,7 +14,8 @@
                         <div class="card-header">
                             <div class="d-flex bd-highlight">
                                 <div class="bd-highlight">
-                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: {{ $thread->title }}
+                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: 
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
                                 </div>
                                 <div class="ml-auto bd-highlight">
                                     {{ $thread->created_at->diffForHumans() }}
