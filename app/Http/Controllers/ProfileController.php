06adcab94e9a4e8cabafe41cf157953cc139e27e
Seigi Takada <seiji.takada@medixserve.com>
2019-08-15 20:43:36 +0800

    Added activity to profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,7 +11,16 @@ class ProfileController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(30)
+            'activities' => $this->getActivity($user)
         ]);
+    }
+
+    public function getActivity(User $user)
+    {
+        $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
+
+        return $activities;
     }
 }
