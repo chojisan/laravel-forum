291eeed5ecd02c92e7e58c05d3b5112a0a94c8db
Seigi Takada <seiji.takada@medixserve.com>
2019-08-15 20:43:56 +0800

    Refactored profile and added activities

---
+++ resources/views/profiles/activities/activity.blade.php
@@ -0,0 +1,10 @@
+<div class="card mb-3">
+    <div class="card-header">
+        <div class="d-flex bd-highlight">
+            {{ $heading }}
+        </div>
+    </div>
+    <div class="card-body">
+        {{ $body }}
+    </div>
+</div>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,26 +9,16 @@
                     <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
                 </h1>
 
-                @foreach ($threads as $thread)
-                    <div class="card mb-3">
-                        <div class="card-header">
-                            <div class="d-flex bd-highlight">
-                                <div class="bd-highlight">
-                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: 
-                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                </div>
-                                <div class="ml-auto bd-highlight">
-                                    {{ $thread->created_at->diffForHumans() }}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            {{ $thread->body }}
-                        </div>
-                    </div>
+                @foreach ($activities as $date => $activity)
+                    <h3 class="page-header">{{ $date }}</h3>
+                    
+                    @foreach ($activity as $record)
+                        @include ("profiles.activities.{$record->type}", ['activity' => $record])
+                    @endforeach
+
                 @endforeach
 
-                {{ $threads->links() }}
+                {{--{{ $threads->links() }}--}}
             </div>
         </div>
     </div>
