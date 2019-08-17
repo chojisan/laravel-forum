e1e8eeb8464dcbc17d7f8a4e65cd564cfeb951ca
Seigi Takada <seiji.takada@medixserve.com>
2019-08-17 13:20:29 +0800

    Removed activity and moved to model feed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Activity;
 
 class ProfileController extends Controller
 {
@@ -11,16 +12,7 @@ class ProfileController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $this->getActivity($user)
+            'activities' => Activity::feed($user)
         ]);
-    }
-
-    public function getActivity(User $user)
-    {
-        $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
-
-        return $activities;
     }
 }
