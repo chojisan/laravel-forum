4142a19b6cc74e1aa95edadfbe3b825d26822425
Seigi Takada <seiji.takada@medixserve.com>
2019-08-19 20:18:55 +0800

    Added unfavorite route

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,6 @@ Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
 Route::patch('/replies/{reply}', 'ReplyController@update');
 Route::delete('/replies/{reply}', 'ReplyController@destroy');
 Route::post('/replies/{reply}/favorites', 'FavoriteController@store');
+Route::delete('/replies/{reply}/favorites', 'FavoriteController@destroy');
 
 Route::get('/profiles/{user}', 'ProfileController@show')->name('profile');
