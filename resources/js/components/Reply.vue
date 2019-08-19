5251b3c72769208660b74c70e5bca91cc2697c37
Seigi Takada <seiji.takada@medixserve.com>
2019-08-19 22:45:20 +0800

    Extracted reply blade to component

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -1,20 +1,69 @@
+<template>
+  <div :id="'reply-' + id" class="card my-3">
+    <div class="card-header">
+      <div class="d-flex bd-highlight">
+        <div class="bd-highlight">
+          <a :href="'/profiles/' + data.owner.name" v-text="data.owner.name"></a>
+          said {{ data.created_at }}...
+        </div>
+
+        <div class="ml-auto bd-highlight" v-if="signedIn">
+          <favorite :reply="data"></favorite>
+        </div>
+      </div>
+    </div>
+
+    <div class="card-body">
+      <div v-if="editing">
+        <div class="form-group">
+          <textarea class="form-control" v-model="body"></textarea>
+        </div>
+
+        <button class="btn btn-primary btn-sm" @click="update">Update</button>
+        <button class="btn btn-danger btn-sm" @click="editing = false">Cancel</button>
+      </div>
+
+      <div v-else v-text="body"></div>
+    </div>
+
+    <div class="card-footer d-flex" v-if="canUpdate">
+      <button class="btn btn-warning btn-sm mr-1" @click="editing = true">Edit</button>
+      <button class="btn btn-danger btn-sm mr-1" @click="destroy">Delete</button>
+    </div>
+  </div>
+</template>
+
 <script>
 import Favorite from "./Favorite.vue";
 
 export default {
-  props: ["attributes"],
+  props: ["data"],
+
   components: {
     Favorite
   },
+
   data() {
     return {
       editing: false,
-      body: this.attributes.body
+      id: this.data.id,
+      body: this.data.body
     };
   },
+
+  computed: {
+    signedIn() {
+      return window.App.signedIn;
+    },
+
+    canUpdate() {
+      return this.authorize(user => this.data.user_id == window.App.user.id);
+    }
+  },
+
   methods: {
     update() {
-      axios.patch("/replies/" + this.attributes.id, {
+      axios.patch("/replies/" + this.data.id, {
         body: this.body
       });
 
@@ -22,12 +71,11 @@ export default {
 
       flash("Updated!");
     },
-    destroy() {
-      axios.delete("/replies/" + this.attributes.id);
 
-      $(this.$el).fadeOut(300, () => {
-        flash("Your reply has been deleted!");
-      });
+    destroy() {
+      axios.delete("/replies/" + this.data.id);
+
+      this.$emit("deleted", this.data.id);
     }
   }
 };
