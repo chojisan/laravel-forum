e7c924423ee8525d2d7fb146c3bf8fad43e77b97
Seigi Takada <seiji.takada@medixserve.com>
2019-08-19 20:17:44 +0800

    Added test for unfavorite

--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -30,6 +30,22 @@ class FavoritesTest extends TestCase
     }
 
     /** @test */
+    public function an_authenticated_user_can_unfavorite_a_reply()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply');
+
+        $this->post("replies/{$reply->id}/favorites");
+
+        $this->assertCount(1, $reply->favorites);
+
+        $this->delete("replies/{$reply->id}/favorites");
+
+        $this->assertCount(0, $reply->fresh()->favorites);
+    }
+
+    /** @test */
     public function an_authenticated_user_may_only_favorite_a_reply_once()
     {
         $this->signIn();
