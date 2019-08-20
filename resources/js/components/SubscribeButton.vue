25d6f709ca44594851af1555dd3e1b68388f4e24
Seigi Takada <seiji.takada@medixserve.com>
2019-08-20 23:05:19 +0800

    Added subscribe button

---
+++ resources/js/components/SubscribeButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :class="classes" @click="subscribe">Subscribe</button>
+</template>
+
+<script>
+export default {
+  props: ["active"],
+
+  data() {
+    return {
+      toggle: this.active
+    };
+  },
+
+  computed: {
+    classes() {
+      return ["btn", this.toggle ? "btn-danger" : "btn-primary"];
+    }
+  },
+
+  methods: {
+    subscribe() {
+      axios[this.toggle ? "delete" : "post"](
+        location.pathname + "/subscriptions"
+      );
+
+      this.toggle = !this.toggle;
+    }
+  }
+};
+</script>
