4d0333f51ce999cb46e1c80e9c8ad13c724bf132
Seigi Takada <seiji.takada@medixserve.com>
2019-08-21 00:05:13 +0800

    Added relations

--- app/ThreadSubscription.php
+++ app/ThreadSubscription.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Notifications\ThreadWasUpdated;
 
 class ThreadSubscription extends Model
 {
     protected $fillable = ['user_id', 'thread_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function thread()
+    {
+        return $this->belongsTo(thread::class);
+    }
+
+    public function notify($reply)
+    {
+        $this->user->notify(new ThreadWasUpdated($this->thread, $reply));
+    }
 }
