ecfba26226f4d6b0a5f97fff6bc7cb52943e0644
Seigi Takada <seiji.takada@medixserve.com>
2019-08-20 23:03:57 +0800

    Appends isSubscribeTo attribute

--- app/Thread.php
+++ app/Thread.php
@@ -14,6 +14,8 @@ class Thread extends Model
 
     protected $with = ['creator', 'channel'];
 
+    protected $appends = ['isSubscribedTo'];
+
     protected static function boot()
     {
         parent::boot();
@@ -48,6 +50,32 @@ class Thread extends Model
     public function addReply($reply)
     {
         return $this->replies()->create($reply);
+    }
+
+    public function subscribe($userId = null)
+    {
+        $this->subscriptions()->create([
+            'user_id' => $userId ?: auth()->id()
+        ]);
+    }
+
+    public function unsubscribe($userId = null)
+    {
+        $this->subscriptions()
+            ->where('user_id', $userId ?: auth()->id())
+            ->delete();
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(ThreadSubscription::class);
+    }
+
+    public function getIsSubscribedToAttribute()
+    {
+        return $this->subscriptions()
+            ->where('user_id', auth()->id())
+            ->exists();
     }
 
     public function scopeFilter($query, $filters)
