cdce85f8571611a2028fb3895943815a866b98e7
Seigi Takada <seiji.takada@medixserve.com>
2019-08-21 15:25:11 +0800

    Added user notification component

---
+++ resources/js/components/UserNotifications.vue
@@ -0,0 +1,44 @@
+<template>
+  <li class="nav-item dropdown" v-if="notifications.length">
+    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+      <span class="fa fa-bell">Notifications</span>
+    </a>
+
+    <ul class="dropdown-menu">
+      <li class="dropdown-item" v-for="notification in notifications" :key="notification.data.id">
+        <a
+          :href="notification.data.link"
+          v-text="notification.data.message"
+          @click="markAsRead(notification)"
+        ></a>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notifications: false
+    };
+  },
+
+  created() {
+    axios
+      .get("/profiles/" + window.App.user.name + "/notifications")
+      .then(response => (this.notifications = response.data));
+  },
+
+  methods: {
+    markAsRead(notification) {
+      axios.delete(
+        "/profiles/" +
+          window.App.user.name +
+          "/notifications/" +
+          notification.id
+      );
+    }
+  }
+};
+</script>
