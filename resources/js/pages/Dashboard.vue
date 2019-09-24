cc36c70f34a28d6f7eb50fcbb74df60a8cd220a1
Seigi Takada <seiji.takada@medixserve.com>
2019-09-24 19:23:03 +0800

    Added dahsboard vue page

---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,30 @@
+<script>
+import LineChart from "../components/LineChart.vue";
+
+export default {
+  components: {
+    LineChart
+  },
+
+  created() {
+    this.fetch("/threads");
+  },
+
+  data() {
+    return {
+      lineChartDataset: false
+    };
+  },
+
+  methods: {
+    fetch(url) {
+      axios.get(url).then(this.refresh);
+    },
+
+    refresh({ data }) {
+      console.log(data);
+      this.lineChartDataset = data;
+    }
+  }
+};
+</script>
