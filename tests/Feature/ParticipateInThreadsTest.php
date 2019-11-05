34ccc829fb86a55a5b3bb0f563486c4600afe285
Seigi Takada <seiji.takada@medixserve.com>
2019-11-05 15:00:20 +0800

    Added test for detecting spam

--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -106,4 +106,18 @@ class ParticipateInForumTest extends TestCase
             'body' => $updatedReply
         ]);
     }
+
+    /** @test */
+    public function replies_that_containts_spam_may_not_be_created()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $reply = make('App\Reply', ['body' => 'Yahoo Customer Support']);
+
+        $this->expectException(\Exception::class);
+
+        $this->post($thread->path() . '/replies', $reply->toArray());
+    }
 }
