581cfe8d30730715acf623cb81e9124dd945595d
Seigi Takada <seiji.takada@medixserve.com>
2019-11-19 20:21:56 +0800

    Added form for uploading avatar

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,6 +9,17 @@
                     <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
                 </h1>
 
+                @can ('update', $profileUser)
+                    <form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
+                        @csrf
+                        <input type="file" name="avatar">
+
+                        <button type="submit" class="btn btn-primary">Add Avatar</button>
+                    </form>
+                @endcan
+
+                <img src="{{ asset($profileUser->avatar_path) }}" height="50" width="50">
+
                 @forelse ($activities as $date => $activity)
                     <h3 class="page-header">{{ $date }}</h3>
                     
