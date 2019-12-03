928f9e5e395abd8fd7d1b855f006cbf97434e970
Seigi Takada <seiji.takada@medixserve.com>
2019-12-03 22:39:46 +0800

    Fixed setUp void

--- tests/Feature/TrendingThreadsTest.php
+++ tests/Feature/TrendingThreadsTest.php
@@ -11,7 +11,7 @@ class TrendingThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
