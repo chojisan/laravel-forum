018922c9065819ad9a1b7ba8cb48d7ace99a5d0f
Seigi Takada <seiji.takada@medixserve.com>
2019-12-03 13:43:51 +0800

    Added avatar form  component

---
+++ resources/js/components/AvatarForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1 v-text="user.name"></h1>
+
+    <form v-if="canUpdate" method="POST" enctype="multipart/form-data">
+      <image-upload name="avatar" @loaded="onLoad"></image-upload>
+    </form>
+
+    <img :src="avatar" height="50" width="50" />
+  </div>
+</template>
+    
+<script>
+import ImageUpload from "./ImageUpload.vue";
+
+export default {
+  props: ["user"],
+
+  components: { ImageUpload },
+
+  data() {
+    return {
+      avatar: this.user.avatar_path
+    };
+  },
+
+  computed: {
+    canUpdate() {
+      return this.authorize(user => user.id === this.user.id);
+    }
+  },
+
+  methods: {
+    onLoad(avatar) {
+      this.avatar = avatar.src;
+
+      this.persist(avatar.file);
+    },
+
+    persist(avatar) {
+      let data = new FormData();
+
+      data.append("avatar", avatar);
+
+      axios
+        .post(`/api/users/$(this.user.name)/avatar`, data)
+        .then(() => flash("Avatar uploaded"));
+    }
+  }
+};
+</script>
