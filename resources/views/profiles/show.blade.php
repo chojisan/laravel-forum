ae1da457f00061ef8197f4e87b4f60ca348f7f36
Seigi Takada <seiji.takada@medixserve.com>
2019-12-03 13:46:23 +0800

    Extracted avatar form upload to vue component

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,21 +4,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <h1>
-                    {{ $profileUser->name }}
-                    <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-                </h1>
 
-                @can ('update', $profileUser)
-                    <form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
-                        @csrf
-                        <input type="file" name="avatar">
-
-                        <button type="submit" class="btn btn-primary">Add Avatar</button>
-                    </form>
-                @endcan
-
-                <img src="{{ $profileUser->avatar() }}" height="50" width="50">
+                <avatar-form :user="{{ $profileUser }}"></avatar-form>
 
                 @forelse ($activities as $date => $activity)
                     <h3 class="page-header">{{ $date }}</h3>
